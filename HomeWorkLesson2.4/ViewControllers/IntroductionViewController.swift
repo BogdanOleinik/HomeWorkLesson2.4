e8822c46c9bc7058bbf5ffcaf4ebf5e06a0230a0
Bogdan Oleinik <bogdan.swedbe@yandex.ru>
2022-01-13 22:15:41 +0300

    Without last screen

---
+++ HomeWorkLesson2.4/ViewControllers/IntroductionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  HomeWorkLesson2.4
+//
+//  Created by Богдан Олейник on 06.01.2022.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    @IBAction func unwind(segue: UIStoryboardSegue) {}
+}
+
---
+++ HomeWorkLesson2.4/ViewControllers/ResultViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ResultViewController.swift
+//  HomeWorkLesson2.4
+//
+//  Created by Богдан Олейник on 12.01.2022.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
